710e97721f27b3ba110e3cff615e8b7f0bf899a4
Ryan Pridgen <encodedrj@gmail.com>
2023-12-30 19:06:15 -0500

    update

--- hello.py
+++ hello.py
@@ -1,11 +1,39 @@
-#save this file as hello.py in your repo
-import tensorflow as tf
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-# Simple hello world using TensorFlow
-hello = tf.constant('Hello, TensorFlow!')
+model_name_or_path = "TheBloke/notux-8x7b-v1-GPTQ"
+# To use a different branch, change revision
+# For example: revision="gptq-4bit-128g-actorder_True"
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path,
+                                             device_map="auto",
+                                             trust_remote_code=False,
+                                             revision="main")
 
-# Start tf session
-sess = tf.Session()
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
 
-# Run the op
-print(sess.run(hello))
+prompt = "Cuentame la historia del niño Cozumeleño que se hizo amigo del lagarto azul."
+system_message = "Eres un asistente de cuentos maravillosos."
+prompt_template=f'''{prompt}
+'''
+
+print("\n\n*** Generate:")
+
+input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
+output = model.generate(inputs=input_ids, temperature=0.7, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=512)
+print(tokenizer.decode(output[0]))
+
+# Inference can also be done using transformers' pipeline
+
+print("*** Pipeline:")
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=512,
+    do_sample=True,
+    temperature=0.7,
+    top_p=0.95,
+    top_k=40,
+    repetition_penalty=1.1
+)
+
+print(pipe(prompt_template)[0]['generated_text'])
